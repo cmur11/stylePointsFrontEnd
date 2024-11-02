cef38900d1d83585e74bc9799b720f39268fc690
Conor Murnane <conormurnane11@gmail.com>
2024-11-02 17:41:54 -0400

    more initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { User } from "./models/user-model";
 function App() {
+  const [data, setData] = useState<User>();
+
+  useEffect(() => {
+    axios
+      .get("/user")
+      .then((res) => setData(res.data))
+      .catch((error) => console.log(error));
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? "Loading..." : data?.firstName}</p>
       </header>
     </div>
   );
